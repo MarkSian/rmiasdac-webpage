2315f438c3f4e1ba99683b5e4b28375be9ba2815
MarkSian <merksianipar@gmail.com>
2025-10-04 10:40:18 -0600

    starter files

---
+++ rmiasdac-webpage/src/App.tsx
@@ -0,0 +1,13 @@
+import LandingSection from './components/LandingSection'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <LandingSection />
+    </>
+  )
+}
+
+export default App
